3f3af0eee6d91875cf43ae2f32ee4c7a1f853b27
Samiunauntor <samiunalimauntor@gmail.com>
2026-02-07 16:44:38 +0600

    completed task 7

--- solution.ts
+++ solution.ts
@@ -82,3 +82,25 @@ function printBookDetails(book: Book): void {
     );
 }
 
+
+
+function getUniqueValues<T extends string | number>(array1: T[], array2: T[]): T[] {
+    const result: T[] = [];
+
+    for (let i = 0; i < array1.length; i++) {
+        if (!result.includes(array1[i])) {
+            result.push(array1[i]);
+        }
+    }
+
+    for (let j = 0; j < array2.length; j++) {
+        if (!result.includes(array2[j])) {
+            result.push(array2[j]);
+        }
+    }
+
+    return result;
+}
+
+
+
