72b06e31a83add7f0cf7d31e38d218292001b07b
Samiunauntor <samiunalimauntor@gmail.com>
2026-02-07 15:56:01 +0600

    completed second task

--- solution.ts
+++ solution.ts
@@ -10,3 +10,19 @@ function formatValue(value: string | number | boolean): string | number | boolea
 
     return !value;
 }
+
+function getLength(value: string | any[]): number {
+
+    if (typeof value === 'string') {
+        return value.length;
+    }
+
+    if (Array.isArray(value)) {
+        return value.length;
+    }
+
+    return 0;
+}
+
+
+
